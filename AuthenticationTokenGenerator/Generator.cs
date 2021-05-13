e737410bd6084ef7ab232cbfd93a0f832d3d5d93
Cinthia Meireles <cinthia@consultalogo.com.br>
2021-05-13 15:27:27 -0300

    Add project files.

---
+++ AuthenticationTokenGenerator/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthenticationTokenGenerator
+{
+    public static class Generator
+    {
+        public static string StringGuidGenerator()
+        {
+            Guid guid = Guid.NewGuid();
+            return guid.ToString();
+        }
+
+        public static string StringTokenGenerator(string guid)
+        {
+            string token = Convert.ToBase64String(Encoding.UTF8.GetBytes(guid));
+            return token;
+        }
+    }
+}
---
+++ AuthenticationTokenGenerator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AuthenticationTokenGenerator
+{
+    class Program
+    {
+        private static void Menu()
+        {
+            Console.WriteLine("Type the number of your option:");
+            Console.WriteLine("1) Generate random GUID and its token");
+            Console.WriteLine("2) Enter a existing GUID and generate its token");
+            Console.WriteLine("0) Exit program");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Menu();
+
+                bool validNumber = int.TryParse(Console.ReadLine(), out choice);
+
+                if (!validNumber)
+                {
+                    Console.WriteLine($"Invalid option, please try again");
+                    Console.WriteLine();
+                    choice = -1;
+                    continue;
+                }
+
+                string guid;
+                string token;
+                switch (choice)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        guid = Generator.StringGuidGenerator();
+                        token = Generator.StringTokenGenerator(guid);
+                        break;
+                    case 2:
+                        guid = Console.ReadLine();
+                        token = Generator.StringTokenGenerator(guid);
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid option, please try again");
+                        Console.WriteLine();
+                        continue;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"GUID: {guid}");
+                Console.WriteLine($"Token: {token}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
